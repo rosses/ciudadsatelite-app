887ed4fcf73632fa5d79f14b7bd292e525354510
Roberto Osses <rosses@gmail.com>
2018-06-24 02:24:47 -0400

    fix ios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -85,7 +85,7 @@ export class MyApp {
       console.log('ready');
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      this.statusBar.styleDefault();
+      this.statusBar.styleLightContent();
       setTimeout(() => {
         this.splashScreen.hide();
       },0);
